acb0264a779bc04f86514c31593afabc26fa1a8b
duffett <duffett@gmail.com>
2016-07-06 22:13:58 -0400

    initial commit

---
+++ file-chunker/Controllers/UploadController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace file_chunker.Controllers
+{
+    public class UploadController : ApiController
+    {
+        public IHttpActionResult Index()
+        {
+            return Ok("index");
+        }
+
+        [HttpGet]
+        public IHttpActionResult Get(string id)
+        {
+            var result = string.Format("The number was...{0}", id);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public IHttpActionResult ReceiveFile(string id)
+        {
+            var result = string.Format("The number was...{0}", id);
+            return Ok(result);
+        }
+    }
+}
--- file-chunker/Global.asax.cs
+++ file-chunker/Global.asax.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Mvc;
+using System.Web.Http;
 using System.Web.Routing;
 
 namespace file_chunker
@@ -11,8 +11,10 @@ namespace file_chunker
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            //AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
         }
     }
 }
